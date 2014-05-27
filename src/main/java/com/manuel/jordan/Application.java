efafc4ceea800a6bf8196d7bfec8d71cac5e4dfe
Manuel Jordan Elera <dr_pompeii@yahoo.coom>
2014-05-27 14:07:28 -0500

    Application class edited

--- src/main/java/com/manuel/jordan/Application.java
+++ src/main/java/com/manuel/jordan/Application.java
@@ -2,8 +2,11 @@ package com.manuel.jordan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import com.manuel.jordan.main.Main;
 
 @Configuration
 @ComponentScan
@@ -11,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    	ApplicationContext context = SpringApplication.run(Application.class, args);
+    	Main.main(context);
     }
 }
